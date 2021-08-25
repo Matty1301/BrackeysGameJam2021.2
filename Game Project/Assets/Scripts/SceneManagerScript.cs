962ab3938136c42fc150b4e743c633ee41f012e5
kz2007 <kh.zgueb@gmail.com>
2021-08-25 13:59:46 +0100

    Menu 3

---
+++ Game Project/Assets/Scripts/SceneManagerScript.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneManagerScript : MonoBehaviour
+{
+    public void ChangeScene(int scene)
+    {
+        SceneManager.LoadScene(scene);
+    }
+    public void QuitGame()
+    {
+        Debug.Log("Quit!!");
+        Application.Quit();
+    }
+}
